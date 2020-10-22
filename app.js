3df94d358757587f36424b9caf020952db1b3b6f
범 <71809367+tiger8012@users.noreply.github.com>
2020-10-22 16:04:38 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+'use strict';
+const express = require('express');
+const hostname = '0.0.0.0';
+const port = 3000;
+const app = express();
+app.get('/',(req,res) => {
+        res.send('hello~~');
+});
+
+app.listen(port,hostname);
+console.log(`Running on http://${hostname}:${port}`);
